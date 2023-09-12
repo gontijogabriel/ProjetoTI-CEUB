ddcdf68d2d3804e7fa8673335b4c5b67441e9144
Gabriel Gontijo <gontijogabr@gmail.com>
2023-09-12 17:41:05 -0300

    update data.py
    sqlite > mysql

--- data.py
+++ data.py
@@ -1,31 +1,49 @@
-from sqlalchemy import create_engine, Column, Integer, String, Numeric
+from sqlalchemy import Column, Integer, String, Numeric
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine  # Importe a função create_engine
+import mysql.connector
 
-# Cria a conexão com o banco de dados
-engine = create_engine('sqlite:///boletos.db', echo=True)
+try:
+    # Estabelece a conexão com o banco de dados
+    conn = mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="root",
+        database="alugacar"
+    )
 
-# Cria a classe base declarativa
-Base = declarative_base()
+    if conn.is_connected():
+        print("Conexão ao banco de dados estabelecida com sucesso!")
 
-# Definindo a classe do modelo
-class Boletos(Base):
-    __tablename__ = 'boletos'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String)
-    valor = Column(Numeric(10, 2))  # Usando tipo Numeric para valores monetários
-    vencimento = Column(String)
-    alerta = Column(String)
-    situacao_pagamento = Column(String)
+    # Cria a classe base declarativa
+    Base = declarative_base()
 
-class Config(Base):
-    __tablename__ = 'email'
-    email = Column(String, primary_key=True)  # Definindo a coluna como chave primária
+    # Definindo a classe do modelo
+    class Boletos(Base):
+        __tablename__ = 'boletos'
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        nome = Column(String(255))  # Especifique o comprimento máximo desejado
+        valor = Column(Numeric(10, 2))  # Usando tipo Numeric para valores monetários
+        vencimento = Column(String(10))  # Especifique o comprimento máximo desejado, por exemplo, 10 caracteres
+        alerta = Column(String(255))  # Especifique o comprimento máximo desejado, por exemplo, 255 caracteres
+        situacao_pagamento = Column(String(255))
 
-# Cria as tabelas no banco de dados
-Base.metadata.create_all(engine)
+
+
+    class Config(Base):
+        __tablename__ = 'email'
+        email = Column(String(255), primary_key=True)  # Definindo a coluna como chave primária
+
+    # Crie as tabelas no banco de dados
+    engine = create_engine('mysql+mysqlconnector://root:root@localhost:3306/alugacar')
+    Base.metadata.create_all(engine)
+
+except mysql.connector.Error as e:
+    print(f"Erro ao conectar ao banco de dados: {e}")
 
 def get_db_connection():
-    Session = sessionmaker(bind=engine)
-    conn = Session()
-    return conn
+    Session = sessionmaker(bind=engine)  # Use o engine criado anteriormente
+    session = Session()
+    return session
